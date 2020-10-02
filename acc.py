d556f4e902a1402eacf9b89b4565dde37a9bf0b2
Grace-Ya-Wen <58238575+Grace-Ya-Wen@users.noreply.github.com>
2020-10-02 17:47:01 +1300

    Update acc.py

--- acc.py
+++ acc.py
@@ -17,6 +17,36 @@ from scipy.ndimage.interpolation import map_coordinates
 from scipy.ndimage.filters import gaussian_filter
 import cv2
 
+def compute_iou(y_pred, y_true):
+    # ytrue, ypred is a flatten vector
+    y_pred = np.rint(y_pred.flatten())
+    y_true = np.rint(y_true.flatten())
+    current = confusion_matrix(y_true, y_pred, labels=[1, 0])
+    # compute mean iou
+    intersection = np.diag(current)
+    ground_truth_set = current.sum(axis=1)
+    predicted_set = current.sum(axis=0)
+    union = ground_truth_set + predicted_set - intersection
+    IoU = intersection / union.astype(np.float32)
+    return np.mean(IoU)
+
+
+def sensitivity(y_pred, y_true):
+    # ytrue, ypred is a flatten vector
+    y_pred = np.rint(y_pred.flatten())
+    y_true = np.rint(y_true.flatten())
+    cm1 = confusion_matrix(y_true, y_pred, labels=[1, 0])
+    sensitivity1 = cm1[0,0]/(cm1[0,0]+cm1[0,1])
+    return sensitivity1
+
+def specificity(y_pred, y_true):
+    # ytrue, ypred is a flatten vector
+    y_pred = np.rint(y_pred.flatten())
+    y_true = np.rint(y_true.flatten())
+    cm1 = confusion_matrix(y_true, y_pred, labels=[1, 0])
+    specificity1 = cm1[1,1]/(cm1[1,0]+cm1[1,1])
+    return specificity1
+
 def accuracy(predict,truth):
     w, h = predict.shape
     count = 0
@@ -27,28 +57,44 @@ def accuracy(predict,truth):
     acc = count/(w*h)
     return acc
 
+def DSC(predict, truth):
 
-predict_files = glob.glob ("/hpc/gwen488/DeepLearning/data/membrane/test/0.01/*.tif")
-#print(len(predict_files))
-#print(predict_files)
-#predict_files.sort(key=lambda x: int(x.split('\\')[1][0:1]))
-predict_files.sort(key=lambda x: int(x.split('_')[0][50:]))
-#print(predict_files)
-#predict_files.sort(key=lambda x: int(x.split('/')[1][:-12]))
+    smooth = 1 
+    #y_true_f = K.flatten(truth) # flatten to 1D
+    #y_pred_f = K.flatten(predict)
+    intersection = K.sum(truth * predict)
+    union = K.sum(truth) + K.sum(predict)
+    #intersection = K.sum(y_true_f * y_pred_f)
+    dice = K.mean((2. * intersection + smooth)/(union + smooth), axis=0)
+    return dice
+
+
+def IoU(predict, truth):
+    w, h = predict.shape
+    inter = 0
+    union = 0
+
+    for i in range(w):
+        for j in range(h):
+            if truth[i][j] == 0 and predict[i][j] == 0:
+                inter+=1
+            if truth[i][j] == 0 or predict[i][j] == 0:
+                union+=1
+    return inter/union
+
+
+predict_files = glob.glob ("you_path_stored_the_prediction")
+
 predict = np.ndarray((len(predict_files),100,100), dtype=np.uint8)
 for i,myFile in enumerate (predict_files):
     image = cv2.imread (myFile)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     image = np.asarray(image, dtype="int32")
-    #image = image.reshape(image.shape+(1,))
     predict[i] = image
 
+files = glob.glob ("you_path_stored_the_gold_standards")
 
-files = glob.glob ("/hpc/gwen488/DeepLearning/data/membrane/test/gold/*.tif")
-#files.sort(key=lambda x: int(x.split('.')[0]))
-files.sort(key=lambda x: int(x.split('.')[0][-1]))
 acc = []
-
 for i in range(len(files)):
     #img = predict[i].reshape(256,256)
     #img = np.uint8(255*img)
